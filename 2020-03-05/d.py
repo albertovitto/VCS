f0e8051b59c23afa06411cc7be261dc19c87ef05
albertovitto <albertovitto16@gmail.com>
2020-03-06 15:30:50 +0100

    checked potential bug

---
+++ 2020-03-05/d.py
@@ -0,0 +1,7 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
