77d6f81312a39c32e1642c4bfdeb727cdfbb2642
albertovitto <albertovitto16@gmail.com>
2020-03-14 22:58:19 +0100

    ok with dimensions of out

--- 2020-03-12 satur arith, color hist, 2D conv/3_2D conv/to_submit.py
+++ 2020-03-12 satur arith, color hist, 2D conv/3_2D conv/to_submit.py
@@ -6,26 +6,20 @@ from skimage import data, io
 import matplotlib.pyplot as plt
 import matplotlib
 from sklearn import preprocessing
-import random
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
 
-print("input")
-n = random.randint(2, 6)
-iC = random.randint(2, 6)
-H = random.randint(10, 20)
-W = random.randint(10, 20)
-print("n  ", n, " iC  ", iC, " H  ", H, " W  ", W)
-input = np.random.rand(n, iC, H, W)
+
+n = input.shape[0]
+iC = input.shape[1]
+H = input.shape[2]
+W = input.shape[3]
+print(n, iC, H, W)
 print(input.shape)
-print("\nkernel")
-oC = random.randint(2, 6)
-kH = random.randint(2, 6)
-kW = random.randint(2, 6)
-print("oC  ", oC, " iC  ", iC, " kH  ", kH, " kW  ", kW)
-kernel = np.random.rand(oC, iC, kH, kW)
+
+oC = kernel.shape[0]
+iC = kernel.shape[1]
+kH = kernel.shape[2]
+kW = kernel.shape[3]
+print(oC, iC, kH, kW)
 print(kernel.shape)
 
 pad = 0
@@ -36,7 +30,4 @@ oW = ((W + (2 * pad) - (dilation * (kW - 1)) - 1) / stride) + 1
 out = np.zeros(
     shape=(int(np.floor(n)), int(np.floor(oC)), int(np.floor(oH)), int(np.floor(oW))),
 )
-print("\nout")
-print("n  ", n, " oC  ", oC, " oH  ", oH, " oW  ", oW)
 print(out.shape)
-
