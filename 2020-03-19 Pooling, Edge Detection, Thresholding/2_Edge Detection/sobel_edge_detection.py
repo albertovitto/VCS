a1e4d857c15b4dd1ef068273172d33dcc810f4df
albertovitto <albertovitto16@gmail.com>
2020-03-20 21:16:42 +0100

    add figures and grid plot to sobel

--- 2020-03-19 Pooling, Edge Detection, Thresholding/2_Edge Detection/sobel_edge_detection.py
+++ 2020-03-19 Pooling, Edge Detection, Thresholding/2_Edge Detection/sobel_edge_detection.py
@@ -17,9 +17,9 @@ from sklearn import preprocessing
 im = data.coins()  # h 303 * w 384 (coins) or h 300 * w 451 * c 3 (chelsea)
 
 fig = plt.figure(figsize=(20, 20))
+rows = 2  # grid 2x3
 columns = 3  # 3 images one next to the other
-rows = 1  # 1 row only
-fig.add_subplot(rows, columns, 1)  # num must be 1 <= num <= 3
+fig.add_subplot(rows, columns, 1)  # last num must be 1 <= num <= 6
 plt.imshow(im)
 
 if len(im.shape) == 3:  # converting to gray scale if image is RGB
@@ -42,7 +42,7 @@ theta = ((theta * 180) / np.pi) / 2  # 0 - 180°
 saturation = np.full_like(theta, 255)  # S (384, 303) value 255
 
 HSV = np.array([theta, saturation, M])
-# cv2.merge([h,s,v]) or np.stack([ , , ]) (3, 384, 303) dtype('float32') value 0-255
+# cv2.merge([h,s,v]) or np.stack([ , , ]) (3, 384, 303) dtype('float32')      values 0-255
 HSV = HSV.astype(np.uint8)  # 32.24 -> 32
 HSV = HSV.swapaxes(0, 2)  # (3, 384, 303) -> (303, 384, 3)
 
@@ -50,6 +50,17 @@ RGB = cv2.cvtColor(HSV, cv2.COLOR_HSV2RGB)  # HSV -> RGB
 
 fig.add_subplot(rows, columns, 2)  # pos 2
 plt.imshow(np.swapaxes(im, 0, 1), cmap="gray")  # (384, 303) -> (303, 384)
-fig.add_subplot(rows, columns, 3)  # pos 3
+
+fig.add_subplot(rows, columns, 3)
+plt.imshow(np.swapaxes(M, 0, 1), cmap="gray")
+
+fig.add_subplot(rows, columns, 4)  # pos 3
 plt.imshow(RGB)  # (303, 384, 3)
+
+fig.add_subplot(rows, columns, 5)
+plt.imshow(np.swapaxes(Gx, 0, 1), cmap="gray")
+
+fig.add_subplot(rows, columns, 6)
+plt.imshow(np.swapaxes(Gy, 0, 1), cmap="gray")
+
 plt.show()
