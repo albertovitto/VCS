e41ba3b0258bddc8d529eeedea6a4e5f2ab69f2f
albertovitto <albertovitto16@gmail.com>
2020-03-21 15:10:45 +0100

    #3 thresholding ok

--- 2020-03-19 Pooling, Edge Detection, Thresholding/4_Otsu Thresholding/otsu_thresholding.py
+++ 2020-03-19 Pooling, Edge Detection, Thresholding/4_Otsu Thresholding/otsu_thresholding.py
@@ -1,12 +1,11 @@
 # Exercise
 # : Given an input grayscale image im (a np.ndarray with shape (H, W) and dtype np.uint8), write a
-# code which performs a binary thresholding of the image at cut value val , and stores the result in out
+# code which computes the Otsu threshold for im and stores the result in out
 # ●
-# out should be another image, with the same shape of im , and with all the pixels greater than the
-# threshold set to 255, all the others set to 0
+# You are not allowed to use any Python package except from Numpy
 # ●
-# Be careful not to modify the original tensor in place: the function should perform the thresholding on
-# a copy of the image
+# Notice: beware of how the threshold is defined in the formulas of the previous slides. Your output
+# should be compliant with our definition of threshold (first slide).
 
 import numpy as np
 import cv2 as cv2
@@ -17,17 +16,17 @@ from sklearn import preprocessing
 from skimage.transform import resize
 import random
 
-# random.seed(0)
 
 im = data.coffee()  # h 400 * w 600 * c 3
 if len(im.shape) == 3:  # converting to gray scale if image is RGB
     im = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)  # h 400 * w 600
 
 fig = plt.figure(figsize=(30, 30))
-rows = 2  # grid 2x3
+rows = 3  # grid 3x3
 columns = 3
 fig.add_subplot(rows, columns, 1)
 plt.imshow(im, cmap="gray")
+
 
 im = resize(
     im,
@@ -45,23 +44,25 @@ im = np.swapaxes(im, 0, 1)  # h 300 * w 200
 fig.add_subplot(rows, columns, 3)
 plt.imshow(im, cmap="gray")
 
-val = random.randint(0, 255)
-print(val)
 
 out = np.copy(im)
-out = (out > val).astype(np.uint8) * 255
-# (out > val) -> matrix of true and false with size of out
-# .astype(np.uint8) convert true to 1, false to 0
-# * 255 convert 1 to 255, 0 to 0 so it's well drawable
 out = np.swapaxes(out, 0, 1)
 fig.add_subplot(rows, columns, 4)
 plt.imshow(out, cmap="gray")
 
-out2 = np.copy(im)
-out2[out2 > val] = 255  # where out2 is over val set 255
-out2[out2 != 255] = 0  # all others cases 0
-out2 = np.swapaxes(out2, 0, 1)
+thr_value, thr_out = cv2.threshold(im, 127, 255, cv2.THRESH_OTSU)
+print(thr_value)
+thr_out = np.swapaxes(thr_out, 0, 1)
 fig.add_subplot(rows, columns, 5)
-plt.imshow(out2, cmap="gray")
+plt.imshow(thr_out, cmap="gray")
+
+# histogram_array = cv2.calcHist(
+#     images=im, channels=[0], mask=None, histSize=[256], ranges=[0, 256]
+# )
+histogram, bin_edges = np.histogram(a=im.ravel(), bins=256, range=[0, 256],)
+fig.add_subplot(rows, columns, 6)
+plt.hist(x=im.ravel(), bins=256, range=[0, 256], density=True, stacked=True)
+b = 0
+
 
 plt.show()
