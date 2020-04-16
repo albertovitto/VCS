fb50824539c9e87bfd53cd329d6defa74dd0f303
albertovitto <albertovitto16@gmail.com>
2020-04-16 16:32:24 +0200

    added sift example

--- 2020-03-26 Geometry , Hough, Template Matching/4_SIFT/sift.py
+++ 2020-03-26 Geometry , Hough, Template Matching/4_SIFT/sift.py
@@ -1,6 +1,8 @@
 import cv2 as cv
 
-image = cv.imread("/home/alberto/Desktop/VCS/2020-03-27 Intro Pytorch/ast.png")
+image = cv.imread(
+    "/home/alberto/Desktop/VCS/2020-03-26 Geometry , Hough, Template Matching/4_SIFT/ast.png"
+)
 gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
 sift = cv.xfeatures2d_SIFT.create()
