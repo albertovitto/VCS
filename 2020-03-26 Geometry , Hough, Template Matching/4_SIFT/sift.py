d4e6fe7fe81e2a89ce1318204ab499f30f962984
albertovitto <albertovitto16@gmail.com>
2020-04-17 15:25:14 +0200

    added refs

--- 2020-03-26 Geometry , Hough, Template Matching/4_SIFT/sift.py
+++ 2020-03-26 Geometry , Hough, Template Matching/4_SIFT/sift.py
@@ -1,5 +1,10 @@
 import cv2 as cv
 
+
+# https://stackoverflow.com/questions/60065707/cant-use-sift-in-opencv-v4-20/60066177#60066177
+
+# https://www.pyimagesearch.com/2018/09/19/pip-install-opencv/
+
 image = cv.imread(
     "/home/alberto/Desktop/VCS/2020-03-26 Geometry , Hough, Template Matching/4_SIFT/ast.png"
 )
